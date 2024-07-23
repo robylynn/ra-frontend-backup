cfa3a2a68944923bb390bcf87260b179fa4bd3dc
Roby Lynn <23561758+robylynn@users.noreply.github.com>
2024-07-22 20:31:00 -0400

    cleanup

--- backend/api/models.py
+++ backend/api/models.py
@@ -33,28 +33,6 @@ class IOPointType(Enum):
     ANALOG_CURRENT_OUTPUT = auto()
     THERMOCOUPLE_INPUT = auto()
 
-# @dataclass
-# class FrontendIOPointConfiguration:
-#     point_type: IOPointType
-#     point_index: int
-
-# @dataclass
-# class FrontendChartTraceConfiguration:
-#     color: str
-#     label: str
-#     y_parameter_name: str
-
-# @dataclass
-# class FrontendChartConfiguration:
-#     chart_id: str
-#     title: str
-#     width_px: int
-#     height_px: int
-#     max_length: int
-#     x_data_value_name: str
-#     y_axis_decimal_places: int
-#     trace_configuration: List[FrontendChartTraceConfiguration]
-
 @dataclass
 class FrontendConfiguration:
     client_id: int
