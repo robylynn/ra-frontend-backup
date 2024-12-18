caa1288bde5026a5540abbdec454fefbaea55ac2
roby <23561758+robylynn@users.noreply.github.com>
2024-12-18 15:52:24 -0500

    update nextauth_url env

--- dashboard/next.config.js
+++ dashboard/next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     NEXTAUTH_SECRET: "sFB6I93i7B/l6eS/Y2JYYHEdQ93Oc4QfUQS+d+kp1Gs=",
-    NEXTAUTH_URL: "http://localhost:3000",
+    // NEXTAUTH_URL: "http://localhost:3000",
   },
   webpack: (config) => {
     config.module.rules.push({
