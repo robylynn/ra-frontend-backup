83ca611b3707561be799c162ea56deae2fcd7787
roby <23561758+robylynn@users.noreply.github.com>
2024-12-18 21:25:36 -0500

    update env

--- dashboard/next.config.production.js
+++ dashboard/next.config.production.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     NEXTAUTH_SECRET: "sFB6I93i7B/l6eS/Y2JYYHEdQ93Oc4QfUQS+d+kp1Gs=",
-    NEXTAUTH_URL: "http://localhost:80",
+    // NEXTAUTH_URL: "http://localhost:80",
   },
   webpack: (config) => {
     config.module.rules.push({
